1b5e00e63ea219b76d6f1121dcd69c2f589cbfe7
Alan Ramos Martinez <shudy1207@gmail.com>
2022-11-16 19:56:47 -0600

    Flappy 3

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const juego = {
     // 1. Agrega la propiedad timerId
-    
+    timerId: 0,
     // 2. Agrega la propiedad gravedad
-    
+    gravedad: 2,
   
     // 3. Agrega el método aleatorio()
     aleatorio: function (min, max) {
+      return Math.round(Math.random() * (max - min) + min);
       // Escribe codigo del método 
     },
   
 
     // 12. Agrega el método loop()
     loop: function () {
-     // Escribe codigo del método
+     bird.efectoGravedad(),
+     bird.dibujar(),
+     juego.verificaColision()
     },
   
     // 13. Agrega el método iniciar()
     iniciar: function() {
-     // Escribe codigo del método
+     document.addEventListener("keyup", bird.mover)
+     juego.timerId = setInterval(juego.loop, 20)
+      // Escribe codigo del método
     }
   
   };
@@ -27,10 +32,10 @@ const juego = {
 
 const bird = {
     // 4. Agrega la propiedad div
-  
+   div: document.querySelector(".bird"),
 
     // 5. Agrega la propiedad bottom
-   
+   bottom: juego.aleatorio(10, 570),
 
     // 6. Agrega la propiedad left
     left: 250,
@@ -39,19 +44,24 @@ const bird = {
     width: 60,
 
     // 8. Agrega la propiedad height
-    
+    height: 45,
   
     // 9. Agrega el método efectoGravedad()
     efectoGravedad: function () {
+      bird.bottom = bird.bottom - juego.gravedad
       // Agrega codigo del método
     },
   
     // 10. Agrega el método dibujar()
     dibujar: function () {
+      bird.div.style.bottom = bird.bottom + "px" 
       //Agrega codigo del método
     },
 
     // 11. Agrega el método mover()
+    mover: function () {
+      bird.bottom = bird.bottom + 40
+    }
 
 }
    
